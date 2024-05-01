c9fdd9f4eca25e15c3084ae5f71807bddd1f9cb3
Juan <jd.portilla10@ciaf.edu.co>
2024-04-30 22:18:52 -0500

    Configuracion del test

---
+++ src/01-sum/sum.js
@@ -0,0 +1,8 @@
+function sum(a, b) {
+    if (a ===undefined||b===undefined){
+        return "Hubo uns error";
+    }
+    return a + b
+}
+
+module.exports = sum;
---
+++ src/01-sum/sum.test.js
@@ -0,0 +1,16 @@
+const  sum = require("./sum")
+
+test('adds 1 + 2 to equal 3', () => {
+    const resultado = sum(1,2)
+    expect(resultado).toBe(3);
+});
+
+test ("add 10 + 10 to equals 20 ", () =>{
+    const resultado = sum (10, 10);
+    expect (resultado).toBe(20);
+});
+
+test ("should return erro if argument are not passed", () =>{
+    const resultado = sum ();
+    expect(resultado).toBe ("Hubo un error");
+});
